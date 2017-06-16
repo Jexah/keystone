5966bd3f77ba096af4c005086a60ecd732fd2b0b
Jexah <ijexah@gmail.com>
2017-06-17 02:07:45 +1000

    Revert "Reverted ):"
    This reverts commit a4b096df3aa84a1238f37fe4e1fbd784b20cbe66.

--- server/bindStaticMiddleware.js
+++ server/bindStaticMiddleware.js
@@ -5,10 +5,16 @@ module.exports = function bindStaticMiddleware (keystone, app) {
 	// when set, we configure the express static middleware
 
 	var staticPaths = keystone.get('static');
+	var staticHostname = keystone.get('static hostname');
 	var staticOptions = keystone.get('static options');
 
 	if (typeof staticPaths === 'string') {
 		staticPaths = [staticPaths];
+	}
+
+	// Use hostname routing for static files to avoid sending cookies, etc.
+	if(staticHostname && req.headers.host.split(':')[0] === staticHostname){
+		app.use('/', express.static(keystone.expandPath(value), staticOptions))
 	}
 
 	if (Array.isArray(staticPaths)) {
